4bd3f6e44c04e814f2e4c7758fcb5bf42c3b5549
theseanything <sean.rankine@me.com>
2015-07-11 14:25:11 +0100

    Initial stable version

---
+++ Sensors/App.xaml.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+namespace Sensors
+{
+    /// Provides application-specific behavior to supplement the default Application class.
+    public sealed partial class App : Application, INotifyPropertyChanged
+    {
+        // if you are using multi-threading someone could unsubscribe between the null check and the calling of the event, so you could still get a  NullReferenceException. To handle that we copy the event handler to a temporary variable
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        // From App.xaml.cs you can create global variables and properties for multiple pages
+        // The following helps with referencing these variables - use App.Current instead of (App)App.Current - (Casting)
+        public static new App Current
+        {
+            get { return (App)Application.Current; }
+        }
+
+        private string statusMessage = "Pair a Microsoft Band with your device and click Run.";
+        public string StatusMessage
+        {
+            get { return statusMessage; }
+            set
+            {
+                statusMessage = value;
+
+                // need to check if not NULL before doing something - i.e. make sure it has changed.
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("StatusMessage"));
+                }
+            }
+        }
+
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+                // Set the default language
+                rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ Sensors/MainPage.cs
@@ -0,0 +1,173 @@
+﻿
+using Microsoft.Band;
+using Microsoft.Band.Sensors;
+using System;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.Storage;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
+
+namespace Sensors
+{
+
+    partial class MainPage
+    {
+        private App viewModel;
+
+        // file storage and string setup
+
+        private StorageFolder folder = ApplicationData.Current.LocalFolder;
+        private StorageFile file;
+        private StringBuilder data = new StringBuilder();
+        private int interval = 20;
+        private int heart = 0;
+
+
+        private void TimeInterval_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(TimeInterval.Text) && Convert.ToInt32(TimeInterval.Text) < 3600 && Convert.ToInt32(TimeInterval.Text) > 0)
+            {
+                ToggleRun.IsEnabled = true;
+                interval = Convert.ToInt32(TimeInterval.Text);
+            }
+            else
+            {
+                ToggleRun.IsEnabled = false;
+            }
+            
+        }
+
+
+
+        private async void Run_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            ToggleRun.IsEnabled = false;
+
+            this.viewModel.StatusMessage = "Loading .";
+
+            file = await folder.CreateFileAsync("datanew.csv", CreationCollisionOption.ReplaceExisting);
+
+            this.viewModel.StatusMessage = "Loading ..";
+
+            try
+            {
+                // Get the list of Microsoft Bands paired to the phone.
+                IBandInfo[] pairedBands = await BandClientManager.Instance.GetBandsAsync();
+                this.viewModel.StatusMessage = "Loading ...";
+
+                if (pairedBands.Length < 1)
+                {
+                    this.viewModel.StatusMessage = "This sample app requires a Microsoft Band paired to your device. Also make sure that you have the latest firmware installed on your Band, as provided by the latest Microsoft Health app.";
+                    //ToggleRun.IsEnabled = true;
+                    return;
+                }
+                Debug.WriteLine("1");
+                this.viewModel.StatusMessage = "Loading ....";
+
+                // Connect to Microsoft Band.
+                using (IBandClient bandClient = await BandClientManager.Instance.ConnectAsync(pairedBands[0]))
+                {
+
+                    // Subscribe to Accelerometer data.
+                    Debug.WriteLine("2");
+                    this.viewModel.StatusMessage = "Connecting ...";
+                    
+                    //HEART RATE
+                    var heart_rate = bandClient.SensorManager.HeartRate;
+                    if (heart_rate.GetCurrentUserConsent() != UserConsent.Granted)
+                    {
+                        await heart_rate.RequestUserConsentAsync();
+                    }
+                    heart_rate.ReadingChanged += HeartRate_ReadingChanged;
+
+                    // GYROSCOPE
+                    var gyroscope = bandClient.SensorManager.Gyroscope;
+                    gyroscope.ReportingInterval = TimeSpan.FromMilliseconds(16);
+                    gyroscope.ReadingChanged += Gyroscope_ReadingChanged;
+                    Debug.WriteLine("3");
+
+                    // ACCELEROMETER
+                    var accelerometer = bandClient.SensorManager.Accelerometer;
+                    accelerometer.ReportingInterval = TimeSpan.FromMilliseconds(16);
+                    accelerometer.ReadingChanged += Accelerometer_ReadingChanged;
+                    Debug.WriteLine("4");
+
+                    //SKIN TEMP
+                    var skinTemp = bandClient.SensorManager.SkinTemperature;
+                    skinTemp.ReadingChanged +=skinTemp_ReadingChanged;
+
+                    // Receive Accelerometer data for a while, then stop the subscription.
+                    this.viewModel.StatusMessage = "Starting to record...";
+                    await accelerometer.StartReadingsAsync();
+                    await heart_rate.StartReadingsAsync();
+                    await gyroscope.StartReadingsAsync();
+                    await skinTemp.StartReadingsAsync();
+
+            
+                    Debug.WriteLine("5");
+                    this.viewModel.StatusMessage = "Recording...";
+                    await Task.Delay(TimeSpan.FromSeconds(interval));
+
+               
+                    await accelerometer.StopReadingsAsync();
+                    await heart_rate.StopReadingsAsync();
+                    await gyroscope.StopReadingsAsync();
+                    await skinTemp.StopReadingsAsync();
+                    Debug.WriteLine("6");
+
+                    await FileIO.WriteTextAsync(file, data.ToString());
+                    this.viewModel.StatusMessage = "File Saved.";
+                    Debug.WriteLine("7");
+                }
+            }
+            catch (Exception ex)
+            {
+                this.viewModel.StatusMessage = ex.ToString();
+            }
+            Debug.WriteLine("8");
+            ToggleRun.IsEnabled = true;
+        }
+
+        private async void skinTemp_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandSkinTemperatureReading> e)
+        {
+            IBandSkinTemperatureReading skintemp = e.SensorReading;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                this.viewModel.StatusMessage = skintemp.Temperature.ToString();
+            }).AsTask();
+        }
+
+        private async void Accelerometer_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandAccelerometerReading> e)
+        {
+            IBandAccelerometerReading accel = e.SensorReading;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                data.Append(string.Format("{0:F2},{1:F2},{2:F2}", accel.AccelerationX, accel.AccelerationY, accel.AccelerationZ));
+            }).AsTask();
+        }
+
+        private async void HeartRate_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandHeartRateReading> e)
+        {
+            IBandHeartRateReading hr = e.SensorReading;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => {
+                heart = hr.HeartRate; 
+            }).AsTask();
+        }
+
+        private async void Gyroscope_ReadingChanged(object sender, BandSensorReadingEventArgs<IBandGyroscopeReading> e)
+        {
+            IBandGyroscopeReading gyro = e.SensorReading;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                data.AppendLine(string.Format(",{0:F3},{1:F3},{2:F3},{3}", gyro.AngularVelocityX, gyro.AngularVelocityY, gyro.AngularVelocityZ, heart));
+            }).AsTask();
+        }
+
+    }
+}
